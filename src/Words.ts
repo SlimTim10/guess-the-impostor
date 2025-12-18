d1aa7bc9c7b5880b455ddf6b77397422a69f1a48
SlimTim10 <slimtim10@gmail.com>
2025-12-18 15:36:56 -0500

    Make nominal type SecretWord

--- src/Words.ts
+++ src/Words.ts
@@ -1,14 +1,16 @@
 import { randomElem } from 'fp-ts/Random'
 import type { ReadonlyNonEmptyArray } from 'fp-ts/ReadonlyNonEmptyArray'
 
-export const wordlist: ReadonlyNonEmptyArray<string> = [
+export type SecretWord = string & { readonly __type: unique symbol }
+
+export const wordlist: ReadonlyNonEmptyArray<SecretWord> = [
   'apple',
   'banana',
-]
+] as unknown as ReadonlyNonEmptyArray<SecretWord>
 
-export const generateRandomSecretWord = (): string => randomElem(wordlist)()
+export const generateRandomSecretWord = (): SecretWord => randomElem(wordlist)()
 
-export const generateNewSecretWord = (w: string): string => {
+export const generateNewSecretWord = (w: SecretWord): SecretWord => {
   const newWord = generateRandomSecretWord()
   if (newWord === w) {
     return generateNewSecretWord(w)
