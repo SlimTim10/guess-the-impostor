03bf5c57281c59885b01a80b8f804364cedda25e
SlimTim10 <slimtim10@gmail.com>
2025-12-18 18:37:39 -0500

    Implement play-again

--- src/Game.ts
+++ src/Game.ts
@@ -1,16 +1,19 @@
 import type { NumberAtLeast3, ValidPlayers } from './ValidPlayers'
-import { createPlayers } from './ValidPlayers'
+import { createPlayers, randomizeValidPlayers } from './ValidPlayers'
 import type { SecretWord } from './Words'
-import { generateRandomSecretWord } from './Words'
+import { generateRandomSecretWord, generateNewSecretWord } from './Words'
 
 export type Game = {
   players: ValidPlayers;
   secretWord: SecretWord;
 }
 
-export const startGame = (x: NumberAtLeast3): Game => {
-  return {
-    players: createPlayers(x),
-    secretWord: generateRandomSecretWord(),
-  }
-}
+export const startGame = (x: NumberAtLeast3): Game => ({
+  players: createPlayers(x),
+  secretWord: generateRandomSecretWord(),
+})
+
+export const playAgain = (g: Game): Game => ({
+  players: randomizeValidPlayers(g.players),
+  secretWord: generateNewSecretWord(g.secretWord),
+})
