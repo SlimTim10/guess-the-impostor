20b69514c6f9dc8870e18f8e85f0981796c6c835
SlimTim10 <slimtim10@gmail.com>
2025-12-18 18:25:27 -0500

    Change import style

--- src/Words.ts
+++ src/Words.ts
@@ -1,14 +1,14 @@
-import { randomElem } from 'fp-ts/Random'
-import type { ReadonlyNonEmptyArray } from 'fp-ts/ReadonlyNonEmptyArray'
+import * as R from 'fp-ts/Random'
+import type RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 
 export type SecretWord = string & { readonly __type: unique symbol }
 
-export const wordlist: ReadonlyNonEmptyArray<SecretWord> = [
+export const wordlist: RNEA.ReadonlyNonEmptyArray<SecretWord> = [
   'apple',
   'banana',
-] as unknown as ReadonlyNonEmptyArray<SecretWord>
+] as unknown as RNEA.ReadonlyNonEmptyArray<SecretWord>
 
-export const generateRandomSecretWord = (): SecretWord => randomElem(wordlist)()
+export const generateRandomSecretWord = (): SecretWord => R.randomElem(wordlist)()
 
 export const generateNewSecretWord = (w: SecretWord): SecretWord => {
   const newWord = generateRandomSecretWord()
