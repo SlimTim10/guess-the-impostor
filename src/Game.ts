f0d171b2825a6d30f15576aba88e60ed12808380
SlimTim10 <slimtim10@gmail.com>
2025-12-18 18:35:42 -0500

    Implement startGame

--- src/Game.ts
+++ src/Game.ts
@@ -1,7 +1,16 @@
-import type { ValidPlayers } from './ValidPlayers'
+import type { NumberAtLeast3, ValidPlayers } from './ValidPlayers'
+import { createPlayers } from './ValidPlayers'
 import type { SecretWord } from './Words'
+import { generateRandomSecretWord } from './Words'
 
 export type Game = {
   players: ValidPlayers;
   secretWord: SecretWord;
+}
+
+export const startGame = (x: NumberAtLeast3): Game => {
+  return {
+    players: createPlayers(x),
+    secretWord: generateRandomSecretWord(),
+  }
 }
