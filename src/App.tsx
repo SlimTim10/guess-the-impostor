fa0a48f9ddf758bf658d8ef0d0a65e859d298d91
SlimTim10 <slimtim10@gmail.com>
2025-12-19 08:51:28 -0500

    Set up views in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,31 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import type { Game } from './Game'
-import type { PlayerRole } from './PlayerRoles'
-import type { Round } from './Rounds'
-import { isRound } from './Rounds'
-import type { ValidPlayers } from './ValidPlayers'
-import { isValidPlayers } from './ValidPlayers'
-import type { SecretWord } from './Words'
-import { generateRandomSecretWord } from './Words'
+/* import type { Game } from './Game'
+ * import type { PlayerRole } from './PlayerRoles'
+ * import type { Round } from './Rounds'
+ * import { isRound } from './Rounds'
+ * import type { ValidPlayers } from './ValidPlayers'
+ * import { isValidPlayers } from './ValidPlayers' */
+import type { View } from './Views'
+/* import type { SecretWord } from './Words'
+ * import { generateRandomSecretWord } from './Words' */
 import Initial from './Views/Initial'
 
 const App = () => {
-  const [players, setPlayers] = useState<null | ValidPlayers>(null)
-  const [round, setRound] = useState<null | Round>(null)
-  const [secretWord, _setSecretWord] = useState<SecretWord>(generateRandomSecretWord())
-  const [game, setGame] = useState<null | Game>(null)
+  const [view, setView] = useState<View>('initial')
 
+  // Testing views
   useEffect(() => {
-    const xs: Array<PlayerRole> = ['keeper', 'keeper', 'impostor']
-    if (isValidPlayers(xs)) {
-      setPlayers(xs)
-    }
-
-    const x: number = 1
-    if (isRound(x)) {
-      setRound(x)
-    }
+    setView('show-role')
   }, [])
 
-  useEffect(() => {
-    if (players !== null) {
-      setGame({
-        players,
-        secretWord,
-      })
-    }
-  }, [players])
-
-  return (
-    <>
-      <div>
-        <Initial />
-      </div>
-      <div className="card">
-        <p>
-          Testing
-        </p>
-        <p>
-          Round: {round}
-        </p>
-        <p>
-          Secret word: {secretWord}
-        </p>
-        <p>
-          Game: <code>{JSON.stringify(game)}</code>
-        </p>
-      </div>
-    </>
-  )
+  switch (view) {
+    case 'initial':
+      return <Initial />
+      break;
+    default:
+      throw 'Error: invalid view'
+  }
 }
 
 export default App
---
+++ src/Views.ts
@@ -0,0 +1,14 @@
+export type View =
+  'initial' |
+  'confirm-restart' |
+  'how-to-play' |
+  'show-role' |
+  'showing-role' |
+  'done-showing-role' |
+  'pass-to-next-player' |
+  'say-a-word' |
+  'voting' |
+  'ask-was-majority-vote-impostor' |
+  'impostor-guessing' |
+  'impostor-wins' |
+  'keepers-win'
