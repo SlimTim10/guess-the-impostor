b857eabafefb1ba83801c46d91795d2adee2c1a7
SlimTim10 <slimtim10@gmail.com>
2025-12-22 14:48:04 -0500

    Add buttons to done-showing-role view

--- src/Views/DoneShowingRole.tsx
+++ src/Views/DoneShowingRole.tsx
@@ -14,12 +14,28 @@ const DoneShowingRole = ({
   openHowToPlay,
   openConfirmRestart,
 }: Props): React.ReactElement => {
+  const handleISawIt = (_e: React.MouseEvent<HTMLButtonElement>): void => {
+    console.log('I saw it')
+  }
+
+  const handleShowRoleAgain = (
+    _e: React.MouseEvent<HTMLButtonElement>,
+  ): void => {
+    console.log('Show role again')
+  }
+
   return (
     <>
       <h1>
         Player {playerTurn} of {game.players.length}
       </h1>
       <h1>Don't let anyone else see the screen!</h1>
+      <div>
+        <button onClick={handleISawIt}>I saw it</button>
+      </div>
+      <div>
+        <button onClick={handleShowRoleAgain}>Show role again</button>
+      </div>
       <div>
         <button onClick={openHowToPlay}>How to play</button>
       </div>
