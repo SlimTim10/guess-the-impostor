266b025d282cdebbb444d2c0c419d01c7e96ca26
SlimTim10 <slimtim10@gmail.com>
2025-12-24 15:45:43 -0500

    Add isView refinement

--- src/Views.ts
+++ src/Views.ts
@@ -11,3 +11,31 @@ export type View =
   | 'impostor-guessing'
   | 'impostor-wins'
   | 'keepers-win'
+
+const assertUnreachable = (_x: never): void => {
+  console.log('assertUnreachable')
+}
+
+export const isView = (str: string): str is View => {
+  const maybeView: View = str as View
+  switch (maybeView) {
+    case 'initial':
+    case 'confirm-restart':
+    case 'show-role':
+    case 'showing-role':
+    case 'done-showing-role':
+    case 'pass-to-next-player':
+    case 'say-a-word':
+    case 'voting':
+    case 'ask-was-majority-vote-impostor':
+    case 'impostor-guessing':
+    case 'impostor-wins':
+    case 'keepers-win':
+      return true
+  }
+
+  // assure exhaustiveness of the switch/case
+  assertUnreachable(maybeView)
+
+  return false
+}
