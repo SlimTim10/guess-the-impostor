adf3ee14bceed50e87d8177287437bb3be7b7d42
SlimTim10 <slimtim10@gmail.com>
2025-12-30 16:06:22 -0500

    Style the AskWasMajorityVoteImpostor view

--- src/Views/AskWasMajorityVoteImpostor.tsx
+++ src/Views/AskWasMajorityVoteImpostor.tsx
@@ -15,20 +15,28 @@ const AskWasMajorityVoteImpostor = ({
 }: Props): React.ReactElement => {
   return (
     <>
-      <h1>Was the majority vote on the impostor?</h1>
-      <p>Impostor, you can now reveal yourself!</p>
-      <div>
-        <button onClick={majorityVoteWasNotImposter}>No</button>
+      <p className="text-lg">Was the majority vote on the impostor?</p>
+      <p className="text-sm">(Impostor, you can now reveal yourself!)</p>
+      <div className="flex gap-x-2">
+        <button onClick={majorityVoteWasNotImposter} className="btn btn-error">
+          No
+        </button>
+        <button onClick={majorityVoteWasImposter} className="btn btn-success">
+          Yes
+        </button>
       </div>
-      <div>
-        <button onClick={majorityVoteWasImposter}>Yes</button>
-      </div>
-      <div>
-        <button onClick={openHowToPlay}>How to play</button>
-      </div>
-      <div>
-        <button onClick={openConfirmRestart}>Restart</button>
-      </div>
+      <button
+        onClick={openHowToPlay}
+        className="btn btn-info btn-xs absolute top-0 right-0 m-2"
+      >
+        How to play
+      </button>
+      <button
+        onClick={openConfirmRestart}
+        className="btn btn-secondary btn-xs absolute top-0 left-0 m-2"
+      >
+        Restart
+      </button>
     </>
   )
 }
