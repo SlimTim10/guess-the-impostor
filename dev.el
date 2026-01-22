d38c3715bfca2b7934023953cf36c7029b37017b
SlimTim10 <slimtim10@gmail.com>
2026-01-22 09:20:08 -0500

    Create nix flake for dev shell

--- dev.el
+++ dev.el
@@ -6,7 +6,7 @@
     (vterm-send-return)))
 
 (let ((project-name "guess-the-impostor-implementation"))
-  (vterm-run-command (concat project-name "-ngrok") "export NIXPKGS_ALLOW_UNFREE=1 && nix shell nixpkgs#ngrok --impure --command ngrok http 5173")
+  (vterm-run-command (concat project-name "-ngrok") "nix develop --command ngrok http 5173")
   (vterm-run-command (concat project-name "-vite") "npm run dev -- --host")
   (vterm-run-command (concat project-name "-tsc") "npm run tsc")
   (vterm-run-command (concat project-name "-prettier") "npm run prettier-watch"))
