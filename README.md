86c872ba0ad9c458cfe7820b081b5df2b77f4496
SlimTim10 <slimtim10@gmail.com>
2026-01-21 19:34:48 -0500

    Make development easier

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Terminal 1: `npm run dev`
 Terminal 2: `npm run tsc`
 
 Terminal 3: `npm run prettier-watch`
+
+## Emacs
+
+Run `dev.el`
---
+++ dev.el
@@ -0,0 +1,13 @@
+(defun vterm-run-command (name cmd)
+  "Run a command in a named vterm terminal."
+  (progn
+    (vterm-other-window name)
+    (vterm-insert cmd)
+    (vterm-send-return)))
+
+(let ((project-name "guess-the-impostor-implementation"))
+  ;; ngrok is not installed globally
+  ;; (vterm-run-command (concat project-name "-ngrok") "ngrok http 5173")
+  (vterm-run-command (concat project-name "-vite") "npm run dev -- --host")
+  (vterm-run-command (concat project-name "-tsc") "npm run tsc")
+  (vterm-run-command (concat project-name "-prettier") "npm run prettier-watch"))
